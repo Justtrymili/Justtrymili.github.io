7ad8202ecb6e517d3ff369f1d56fd00e2e2faad5
Justtrymili <99335440+Justtrymili@users.noreply.github.com>
2022-02-17 18:32:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-server: {
-        origin: 'http://localhost:3000/frontend/',
-        host: '0.0.0.0',
-        fs: {
-            strict: true,
-        }
-},
+import { defineConfig } from 'vite'
+import three from '@vitejs/plugin-vanilla'
+
+
+export default defineConfig({
+        base: '/Justtrymili.github.io/',
+        plugins:[three()]
+})
+
